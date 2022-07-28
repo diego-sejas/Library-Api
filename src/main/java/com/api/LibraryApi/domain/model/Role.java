c47140b7b8fbd67e174e071182b2ccfdf53ed39d
Diego A. Sejas <dsejas2015@gmail.com>
2022-07-28 18:19:59 -0300

    User ,Role - Entities

---
+++ src/main/java/com/api/LibraryApi/domain/model/Role.java
@@ -0,0 +1,32 @@
+package com.api.LibraryApi.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name can not be emtpy")
+    @Column(name = "name", length = 40, nullable = false)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+}
